2f103f2e330cfea37fb9c1e459fcb6a05a49bf9f
Programmer1 <frc3484@gmail.com>
2023-01-28 09:34:28 -0500

    - Merged Code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -4,6 +4,7 @@
 
 #include "Robot.h"
 #include "constants.h"
+#include "FRC3484_Lib/utils/SC_Functions.h"
 
 #include "frc/PneumaticsModuleType.h"
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -14,24 +15,95 @@
 #include "networktables/NetworkTableInstance.h"
 
 using namespace frc;
+using namespace SC;
 
 void Robot::RobotInit() 
 {
-  	GP1_Driver = new XboxController(C_DRIVER_USB);
+	GP1_Driver = new XboxController(/*USB Port*/ 0);
+	BB_GameDevice = new GenericHID(/*USB Port*/ 1);
 
-	_drivetrain = new X23_Drivetrain();
-
+  	x23_drive = new X23_Drivetrain(C_X22_TRACK_WIDTH, C_HI_GEAR_MAX_SPEED, 90_deg_per_s,
+                                   std::make_tuple<int, int>(C_FX_LEFT_MASTER, C_FX_LEFT_SLAVE),
+                                   std::make_tuple<int, int>(C_FX_RIGHT_MASTER, C_FX_RIGHT_SLAVE),
+                                   SC::SC_DoubleSolenoid{C_PCM, frc::PneumaticsModuleType::REVPH, C_DRIVE_SOL, 1});
 }
-void Robot::RobotPeriodic() {}
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want to run during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {
+	frc2::CommandScheduler::GetInstance().Run();
+}
 
+/**
+ * This function is called once each time the robot enters Disabled mode. You
+ * can use it to reset any subsystem information you want to clear when the
+ * robot is disabled.
+ */
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+/**
+ * This autonomous runs the autonomous command selected by your {@link
+ * RobotContainer} class.
+ */
 void Robot::AutonomousInit() {}
+
 void Robot::AutonomousPeriodic() {}
 
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopInit() {
+  // This makes sure that the autonomous stops running when
+  // teleop starts running. If you want the autonomous to
+  // continue until interrupted by another command, remove
+  // this line or comment it out.
+	if (m_autonomousCommand != nullptr) {
+		m_autonomousCommand->Cancel();
+		m_autonomousCommand = nullptr;
+	}
 
-void Robot::DisabledInit() {}
-void Robot::DisabledPeriodic() {}
+}
+
+/**
+ * This function is called periodically during operator control.
+ */
+void Robot::TeleopPeriodic() 
+{
+	/*======================*/
+	/*====Driver Controls===*/
+	/*======================*/
+
+	if(GP1_Driver->GetRightBumper())
+	{
+		// Fine control mode; Scales driver input to smaller range for finer control
+		throttleDemand = F_Scale(-100.0, 100.0, Throttle_Range_Fine, -GP1_Driver->GetLeftY());
+		turnDemand = F_Scale(-100.0, 100.0, Throttle_Range_Fine, GP1_Driver->GetLeftX());
+	}
+	else
+	{
+		// Normal control mode
+		throttleDemand = F_Scale(-100.0, 100.0, Throttle_Range_Normal, -GP1_Driver->GetLeftY());
+		turnDemand = F_Scale(-100.0, 100.0, Throttle_Range_Normal, GP1_Driver->GetLeftX());
+		// throttleDemand = -GP1_Driver->GetLeftY();
+		// turnDemand = GP1_Driver->GetLeftX();
+	}
+
+	forceLowGear = GP1_Driver->GetAButton() || GP1_Driver->GetRightBumper();
+
+	x23_drive->Drive(SC::F_Deadband(throttleDemand, C_DRIVE_DEADBAND),
+					 SC::F_Deadband(turnDemand, C_DRIVE_DEADBAND),
+                   	 forceLowGear);
+  
+}
+
+/**
+ * This function is called periodically during test mode.
+ */
+void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
