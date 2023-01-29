38c31ee3095e035bcc9c3ca79aa2ed41d2f3061d
StarKing117 <flipbaker23@yahoo.com>
2023-01-29 00:01:28 -0500

    I added back in a Commented out Section

--- src/main/cpp/Subsystems/X23_Elevator.cpp
+++ src/main/cpp/Subsystems/X23_Elevator.cpp
@@ -83,5 +83,52 @@ void X23_Elevator::Elevate(double TiltAngle, double ElevatorHeight)
 
 void X23_Elevator::ToggleClaw(bool ClawToggleClose, bool ClawTiltDown)
 {
-  
+    /*
+if((this-> != NULL))
+    {
+        if((this->_dbnc_s1e != NULL) && (this->_dbnc_s1e != NULL)) //&& (this->_dbnc_s2e != NULL) && (this->_dbnc_s2c != NULL))
+        {
+            this->rTrig_s1e->Check(this->_dbnc_s1e->Calculate(Stage1_Ext));
+            this->rTrig_s1c->Check(this->_dbnc_s1c->Calculate(Stage1_Claw));
+            // this->rTrig_s2e->Check(this->_dbnc_s2e->Calculate(Stage2_Ext));
+            // this->rTrig_s2c->Check(this->_dbnc_s2c->Calculate(Stage2_Claw));
+        }
+        else
+        {
+            // Debouncer's are not instantiated, read button change directly.
+            this->rTrig_s1e->Check(Stage1_Ext);
+            this->rTrig_s1c->Check(Stage1_Claw);
+            // this->rTrig_s2e->Check(Stage2_Ext);
+            // this->rTrig_s2c->Check(Stage2_Claw);
+        }
+
+        this->stage1_ext_state  = (this->stage1_ext_state && (!this->rTrig_s1e->Q))     || (!this->stage1_ext_state && this->rTrig_s1e->Q);
+        this->stage1_claw_state = (this->stage1_claw_state && (!this->rTrig_s1c->Q))    || (!this->stage1_claw_state && this->rTrig_s1c->Q);
+        // this->stage2_ext_state  = (this->stage2_ext_state && (!this->rTrig_s2e->Q))     || (!this->stage2_ext_state && this->rTrig_s2e->Q);
+        // this->stage2_claw_state = (this->stage2_claw_state && (!this->rTrig_s2c->Q))    || (!this->stage2_claw_state && this->rTrig_s2c->Q);
+    }
+    else
+    {
+        if((this->_dbnc_s1e != NULL) && (this->_dbnc_s1e != NULL)) // && (this->_dbnc_s2e != NULL) && (this->_dbnc_s2c != NULL))
+        {
+            bool s1e, s1c, s2e, s2c;
+
+            s1e = this->_dbnc_s1e->Calculate(Stage1_Ext);
+            s1c = this->_dbnc_s1c->Calculate(Stage1_Claw);
+
+
+            this->stage1_ext_state = (this->stage1_ext_state && !s1e) || (!this->stage1_ext_state && s1e);
+            this->stage1_claw_state = (this->stage1_claw_state && !s1c) || (!this->stage1_claw_state && s1c);
+             }
+        else
+        {
+            this->stage1_ext_state = (this->stage1_ext_state && !Stage1_Ext) || (!this->stage1_ext_state && Stage1_Ext);
+            this->stage1_claw_state = (this->stage1_claw_state && !Stage1_Claw) || (!this->stage1_claw_state && Stage1_Claw);
+                   }
+    }
+
+    if(this->_stage1_ext != NULL) { this->_stage1_ext->Set(this->stage1_ext_state); }
+    if(this->_stage1_grab != NULL) { this->_stage1_grab->Set(this->stage1_claw_state); }
+
+    */
 }
