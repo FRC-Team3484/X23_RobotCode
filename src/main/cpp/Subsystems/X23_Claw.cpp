ab151085b1197feaea5743660aa056eb2b277f45
tacitquazer <71609909+temcclel@users.noreply.github.com>
2023-02-11 10:03:14 -0500

    we added the clah src and head files.

---
+++ src/main/cpp/Subsystems/X23_Claw.cpp
@@ -0,0 +1,7 @@
+#include "Subsystems/X23_Claw.h"
+
+#include "Constants.h" 
+#include "Globals.h"
+using namespace frc;
+using namespace SC;
+using namespace nt;
---
+++ src/main/include/Subsystems/X23_Claw.h
@@ -0,0 +1,36 @@
+#ifndef X23_Claw_h
+#define X23_Claw_h
+
+#include "FRC3484_Lib/components/SC_Limelight.h"
+#include "FRC3484_Lib/components/SC_ColorSensor.h"
+#include "FRC3484_Lib/components/SC_PID.h"
+
+#include "FRC3484_Lib/utils/SC_Datatypes.h"
+#include "FRC3484_Lib/utils/SC_Functions.h"
+
+#include "ctre/phoenix/motorcontrol/can/TalonFX.h"
+#include "ctre/phoenix/motorcontrol/can/VictorSPX.h"
+
+#include "frc/Solenoid.h"
+#include "frc/DigitalInput.h"
+#include "frc/filter/Debouncer.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
